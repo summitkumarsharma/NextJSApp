0c83b823effc02265fde168de2e040805a0c33a7
Sumit Sharma <65065936+summitkumarsharma@users.noreply.github.com>
2024-02-08 17:15:57 +0530

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+// import { Inter } from 'next/font/google';
+ 
+// export const inter = Inter({ subsets: ['latin'] });
+
+
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+    weight: '400',
+    subsets: ['latin'],
+    display: 'swap',
+  })
